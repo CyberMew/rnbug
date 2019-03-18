eb75609dac654f7f6ac4fee75de3259e87fbf831
CyberMew <CyberMew@gmail.com>
2019-03-18 16:16:09 +0800

    cleanup commit

--- Bug/App.js
+++ Bug/App.js
@@ -9,13 +9,6 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text} from 'react-native';
 import {Panel} from "./components/Panel.js";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component {
   render() {
     return (
